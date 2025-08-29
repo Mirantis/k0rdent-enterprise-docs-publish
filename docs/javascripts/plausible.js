71df87288399dd7e69cf96e63dc76ff6fa0f17e1
John Jainschigg <jjainschigg@mirantis.com>
2025-08-28 20:04:17 -0400

    Create plausible.js

---
+++ docs/javascripts/plausible.js
@@ -0,0 +1,7 @@
+(function() {
+  var d = document, s = d.createElement('script');
+  s.defer = true;
+  s.setAttribute('data-domain', 'docs.k0rdent-enterprise.io');
+  s.src = 'https://plausible.io/js/script.js';
+  d.head.appendChild(s);
+})();
